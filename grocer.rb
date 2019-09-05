8f868ff1415a4593f31cf7dccda3cc4a060494c2
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-05 21:19:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+    cart.each do |ele_hash|
+      
+        ele_name = ele_hash.keys[0]
+  
+        if final_hash.has_key?(ele_name)
+          final_hash[ele_name][:count] += 1
+        else
+          final_hash[ele_name] = {
+            price: ele_hash[ele_name][:price],
+            clearance: ele_hash[ele_name][:clearance],
+            count: 1
+          }
+      end
+    end
+    final_hash
 end
 
 def apply_coupons(cart, coupons)
