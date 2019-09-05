aeff2fb6eb9c96643abda808e1c4a79114d34fb4
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-05 22:53:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,5 +47,11 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # code here
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  
+  total = applied_discount.reduce({0}) { |sum, (key, value)| acc += value[:price] * value[:count] }
+
+  total > 100 ? total * 0.9 : total
 end
